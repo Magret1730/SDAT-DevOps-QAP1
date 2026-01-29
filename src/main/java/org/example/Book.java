440643a45e838a1a244f7cf75fdf79eb45df6541
Magret <belloabiodun17@gmail.com>
2026-01-29 15:03:36 -0330

    Added Book object

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,59 @@
+package org.example;
+
+//o Features: Book issue/return, search catalog, user management.
+
+public class Book {
+    public String title;
+    public String author;
+    public String isbn;
+    public boolean isAvailable;
+
+    public Book(String title, String author, String isbn, boolean isAvailable) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.isAvailable = isAvailable;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
+}
